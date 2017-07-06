c91657d5d54c6c0d965c2216eb73b9eeaa76a6c7
Dreaded_X <tim.huizinga@gmail.com>
2017-07-06 20:46:54 +0200

    Manual patches for screenshots
    Change-Id: I0bc160464a1bfc157d2b34e0364e714c9c849d78

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3709,8 +3709,20 @@ public final class Settings {
           */
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
-		 public static final String TWEAKS_VISUALIZER_COLOR = "tweaks_visualizer_color";
-		 public static final String TWEAKS_VISUALIZER_CUSTOM_COLOR = "tweaks_visualizer_custom_color";
+         public static final String TWEAKS_VISUALIZER_COLOR = "tweaks_visualizer_color";
+         public static final String TWEAKS_VISUALIZER_CUSTOM_COLOR = "tweaks_visualizer_custom_color";
+
+         /**
+          * Define if screenshot crop & share is active
+          * @hide
+          */
+         public static final String SCREENSHOT_CROP_AND_SHARE = "screenshot_crop_and_share";
+ 
+         /**
+          * Define the crop behavior of screenshot crop & share
+          * @hide
+          */
+         public static final String SCREENSHOT_CROP_BEHAVIOR = "screenshot_crop_behavior";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
